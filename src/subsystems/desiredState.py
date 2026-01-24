b2cb1a66f81e2e57a0e0e0ecf7a75414d5600e7c
TinyIg <sleepyjr141@gmail.com>
2026-01-24 12:05:05 -0600

    Fully reverted (mostly)

---
+++ src/subsystems/desiredState.py
@@ -0,0 +1,24 @@
+from dataclasses import dataclass
+from subsystems.networkTablesMixin import NetworkTablesMixin
+from wpimath.kinematics import ChassisSpeeds
+from wpimath.units import meters_per_second as MPS
+
+
+@dataclass
+class DesiredState(NetworkTablesMixin):
+    fieldSpeeds: ChassisSpeeds
+    abtainableMaxSpeed: MPS
+    AButton: bool
+    BButton: bool
+
+    def __post_init__(self) -> None:
+        super().__init__()
+
+    def publish(self) -> None:
+        vx = float(self.fieldSpeeds.vx)
+        vy = float(self.fieldSpeeds.vy)
+        omega = float(self.fieldSpeeds.omega)
+
+        self.publishDouble("vx", vx, "FieldSpeeds")
+        self.publishDouble("vy", vy, "FieldSpeeds")
+        self.publishDouble("omega", omega, "FieldSpeeds")
