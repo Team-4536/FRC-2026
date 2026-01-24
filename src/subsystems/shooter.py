0847a1cde9d0b38490f5e68c33e675ea45fda406
CallmeElodie <lfam5718@gmail.com>
2026-01-24 01:53:09 -0600

    added function for calculating velocity

--- src/subsystems/shooter.py
+++ src/subsystems/shooter.py
@@ -2,6 +2,7 @@ from subsystems.subsystem import Subsystem
 from subsystems.motor import RevMotor
 from subsystems.desiredState import DesiredState
 from ntcore import NetworkTableInstance
+import math
 
 # motor vars do not include real device id at this time (1-13)
 # all RPM values must be tested on the robot and adjusted acoringly
@@ -13,6 +14,7 @@ class Shooter(Subsystem):
         self.table = NetworkTableInstance.getDefault().getTable("telemetry")
 
         self.hubDistance = 2  # this will later be equal to the hypotenuse of Odometry and the Hub position
+        self.wheelRadius = 0.1  # this will later be equal to the radius of the wheels build decides to use
 
         self.revingMotor = RevMotor(deviceID=8)
         self.shooterMotor = RevMotor(deviceID=9)
@@ -26,13 +28,18 @@ class Shooter(Subsystem):
         self.table.putNumber("shootShooter", ds.shootShooter)
 
         if ds.revShooter > 0.1:
-            self.revingMotor.setVelocity(
-                (100 * (ds.revShooter + 1)) * self.hubDistance
-            )  # relationship between distace and revingMotor must be calibrated
-        elif ds.shootShooter and self.revingMotor.getEncoder().getVelocity == 1:
-            self.shooterMotor.setVelocity(120)
+            self.revingMotor.setVelocity(self._calculateVelocity())
+        elif ds.shootShooter and self.revingMotor.getEncoder().getVelocity() == 1:
+            self.shooterMotor.setVelocity(self._calculateVelocity())
         else:
             self.disabled()
+
+    def _calculateVelocity(self) -> float:
+        self.velocityMps = math.sqrt(
+            (9.8 * 0.9652**2)
+            / (2 * math.cos(70) * (self.hubDistance * math.tan(70) - 0.9652))
+        )
+        return self.velocityMps / self.wheelRadius
 
     def disabled(self) -> None:
         self.shooterMotor.setVelocity(0)
