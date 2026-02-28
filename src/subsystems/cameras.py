632912ec13aecd52456df9fb0dbd1d89b9206d1a
Jack S <lolbetiwontusethimuch@gmail.com>
2026-02-28 09:20:22 -0600

    fixed cameras slightly

--- src/subsystems/cameras.py
+++ src/subsystems/cameras.py
@@ -143,7 +143,7 @@ class CameraManager(Subsystem):
         self.photonCameraRight = photonCameraClass(
             "Camera1",
             15,
-            -30,
+            -33,
             inchesToMeters(27 / 2) - (9.1 / 100),
             inchesToMeters(27 / 2) - (4.4 / 100),
             27.5 / 100,
@@ -151,7 +151,7 @@ class CameraManager(Subsystem):
         self.photonCameraLeft = photonCameraClass(
             "Camera2",
             15,
-            30,
+            27,
             inchesToMeters(27 / 2) - (15.3 / 100),
             inchesToMeters(27 / 2) - (4.4 / 100),
             27.5 / 100,
