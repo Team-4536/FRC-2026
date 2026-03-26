6b2fdce7ae0c3a589487665c931b497712a13dfe
Bea Martin <bmartin010@stpaul.k12.mn.us>
2026-03-26 15:55:03 -0500

    fixed intake

--- src/subsystems/intake.py
+++ src/subsystems/intake.py
@@ -59,7 +59,7 @@ class Intake(Subsystem):
 
     def periodic(self, robotState: RobotState) -> RobotState:
         self.robotState = RobotState
-        self.motorForwardSetpoint = 0.7
+        self.motorForwardSetpoint = -0.7
         # -max(
         #     min(self.getFloat("intake_speed (0 to 1)", default=0.0), 1.0), 0
         # )
@@ -67,7 +67,7 @@ class Intake(Subsystem):
         # max(
         #     min(self.getFloat("reverse_speed (0 to 1)", default=0.0), 1.0), 0
         # )
-        self.indexerSetpoint = 0.5
+        self.indexerSetpoint = -0.5
         # -max(
         #     min(self.getFloat("indexer_speed (0 to 1)", default=0.0), 1.0), 0
         # )
