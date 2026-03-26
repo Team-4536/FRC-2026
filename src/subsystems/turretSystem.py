d4f96aba50d08c3b2301715fee9d8e6bc1e490fd
emmett c <emmett@christensons.net>
2026-03-26 10:43:13 -0500

    lowered pitch speed

--- src/subsystems/turretSystem.py
+++ src/subsystems/turretSystem.py
@@ -359,10 +359,10 @@ class Turret(Subsystem):
                 self.yawVelocity = 1
 
             if setPoint > 270 or setPoint < 90:
-                self.pitchVelocity = -1
+                self.pitchVelocity = -0.5
 
             elif setPoint > 90 and setPoint < 270:
-                self.pitchVelocity = 1
+                self.pitchVelocity = 0.5
 
         if not (self.yawVelocity == 0) and not (
             self.relativeYawSetpoint == self.dontOverdoItYaw(self.relativeYawSetpoint)
