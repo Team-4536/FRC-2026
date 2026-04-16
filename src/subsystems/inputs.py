a643a22071a99ae1f69bf3838d719841acdba28f
Jack S <lolbetiwontusethimuch@gmail.com>
2026-04-16 07:28:16 -0500

    lowkey forgot to push this

--- src/subsystems/inputs.py
+++ src/subsystems/inputs.py
@@ -2,17 +2,18 @@ from math import tau
 from subsystems.robotState import RobotState
 from subsystems.subsystem import Subsystem
 from subsystems.utils import CircularScalar, lerp, Scalar
-from wpilib import XboxController
+
 from wpimath.kinematics import ChassisSpeeds
 from wpimath.units import meters_per_second
+from wpilib import Joystick
 
 
 class Inputs(Subsystem):
     LOW_MAX_ABTAINABLE_SPEED: meters_per_second = 1.5
     MAX_ABTAINABLE_SPEED: meters_per_second = 5
 
-    _driveCtrlr: XboxController
-    _mechCtrlr: XboxController
+    _leftCtrlr: Joystick
+    _rightCtrlr: Joystick
 
     _linearDriveScalar: Scalar
     _circularDriveScalar: CircularScalar
@@ -21,8 +22,8 @@ class Inputs(Subsystem):
     def __init__(self, drivePort: int = 0, mechPort: int = 1) -> None:
         super().__init__()
 
-        self._driveCtrlr = XboxController(drivePort)
-        self._mechCtrlr = XboxController(mechPort)
+        self._leftCtrlr = Joystick(drivePort)
+        self._rightCtrlr = Joystick(mechPort)
 
         self.polarity = True
 
@@ -51,34 +52,36 @@ class Inputs(Subsystem):
         maxSpeed = lerp(
             self.LOW_MAX_ABTAINABLE_SPEED,
             self.MAX_ABTAINABLE_SPEED,
-            min(1.0, self._driveCtrlr.getRightTriggerAxis() / 0.9),
+            min(1.0, self._leftCtrlr.getRawAxis(4) / 0.9),
         )
         robotState.fieldSpeeds = self._calculateDrive(maxSpeed)
-        robotState.resetGyro = self._driveCtrlr.getStartButtonPressed()
+        robotState.resetGyro = self._rightCtrlr.getRawButtonPressed(5)
         self.polarity = (
-            not self.polarity if self._driveCtrlr.getXButtonPressed() else self.polarity
+            not self.polarity
+            if self._rightCtrlr.getRawButtonPressed(6)
+            else self.polarity
         )
 
         # Climb Controls
-        robotState.climbUp = self._driveCtrlr.getYButton()
-        robotState.climbDown = self._driveCtrlr.getAButton()
+        robotState.climbUp = self._leftCtrlr.getRawButton(10)
+        robotState.climbDown = self._leftCtrlr.getRawButtonPressed(12)
 
         # Turret Controls
-        robotState.turretSwitchMode = self._mechCtrlr.getYButtonPressed()
-        robotState.turretManualSetpoint = self._mechCtrlr.getPOV()
-        robotState.turretSwitchTarget = self._mechCtrlr.getXButtonPressed()
-        robotState.revSpeed = self._mechCtrlr.getRightTriggerAxis()
-        robotState.kickShooter = self._mechCtrlr.getRightBumper()
+        robotState.turretSwitchMode = self._leftCtrlr.getRawButtonPressed(5)
+        robotState.turretManualSetpoint = self._leftCtrlr.getPOV()
+        robotState.turretSwitchTarget = self._leftCtrlr.getRawButtonPressed(3)
+        robotState.revSpeed = self._rightCtrlr.getRawButton(1)
+        robotState.kickShooter = self._leftCtrlr.getRawButton(1)
 
         # Intake Controls
-        robotState.initialIntake = self._mechCtrlr.getAButton()
-        robotState.intakeIndexer = self._mechCtrlr.getRightBumper()
-        robotState.intakeEject = self._mechCtrlr.getBButton()
-        robotState.indexerEject = self._mechCtrlr.getBButton()
-        robotState.intakePosYAxis = self._mechCtrlr.getRightY()
-        robotState.intakeModeLeftBumperPressed = self._mechCtrlr.getLeftBumperPressed()
-        robotState.ejectAll = self._mechCtrlr.getLeftTriggerAxis()
-        robotState.intakePos = self._mechCtrlr.getBackButtonPressed()
+        robotState.initialIntake = self._rightCtrlr.getRawButton(3)
+        robotState.intakeIndexer = self._rightCtrlr.getRawButton(7)
+        robotState.intakeEject = self._leftCtrlr.getRawButton(3)
+        robotState.indexerEject = self._leftCtrlr.getRawButton(4)
+        robotState.intakePosYAxis = self._rightCtrlr.getPOV()
+        robotState.intakeModeLeftBumperPressed = self._rightCtrlr.getRawButtonPressed(7)
+        robotState.ejectAll = self._rightCtrlr.getRawButton(4)
+        robotState.intakePos = self._rightCtrlr.getRawButtonPressed(11)
 
         return robotState
 
@@ -94,7 +97,7 @@ class Inputs(Subsystem):
 
         if not self.proxyControlMode:
             vx, vy = self._circularDriveScalar(
-                x=-self._driveCtrlr.getLeftY(), y=-self._driveCtrlr.getLeftX()
+                x=-self._rightCtrlr.getRawAxis(1), y=-self._leftCtrlr.getRawAxis(2)
             )
             if not self.polarity:
                 vx, vy = -vx, -vy
@@ -105,6 +108,6 @@ class Inputs(Subsystem):
             # )
             vx, vy = 0, 0
 
-        omega = self._linearDriveScalar(-self._driveCtrlr.getRightX())
+        omega = self._linearDriveScalar(-self._rightCtrlr.getRawAxis(3))
 
         return ChassisSpeeds(vx, vy, omega)
